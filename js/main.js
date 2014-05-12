7582bad8dd31ffdc41bc1da75e2a919ef538d902
dyep49 <dyep49@gmail.com>
2014-05-12 12:38:22 -0400

    randomize measures

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
+
+  var stairwayArray = [1,2,3,4]
+  stairwayArray = _.shuffle(stairwayArray)
+
+  var spiritArray = [1,2,3,4]
+  spiritArray = _.shuffle(spiritArray)
+
   for(var i=1;i<5;i++) {
+    var num = stairwayArray.pop()
+
     var r = Math.floor(255 * Math.random())
     var g = Math.floor(255 * Math.random())
     var b = Math.floor(255 * Math.random())
 
-    var el = $('<div class="drag-item"><img src="img/spirit' + i + '.png"><audio controls><source src="audio/spirit' + i +'.wav" type="audio/wav">Your browser does not support the audio element.</audio></div>')    
+    var el = $('<div class="drag-item"><img src="img/spirit' + num + '.png"><audio controls><source src="audio/spirit' + num +'.wav" type="audio/wav">Your browser does not support the audio element.</audio></div>')    
     $('#pile').append(el)
 
     el.css('background', "rgb(" + r + ',' + g + ',' + b + ')')
-
-    jQuery.data(el[0], 'number', i)
 
     el.dblclick(function() { 
       this.children[1].play()
@@ -25,16 +32,17 @@ $(document).ready(function() {
   })
 
   for(var i=1;i<5;i++) {
+    var num = spiritArray.pop()
+
+
     var r = Math.floor(255 * Math.random())
     var g = Math.floor(255 * Math.random())
     var b = Math.floor(255 * Math.random())
 
-    var el = $('<div class="drag-item"><img src="img/stairway' + i + '.png"><audio controls><source src="audio/stairway' + i +'.wav" type="audio/wav">Your browser does not support the audio element.</audio></div>')    
+    var el = $('<div class="drag-item"><img src="img/stairway' + num + '.png"><audio controls><source src="audio/stairway' + num +'.wav" type="audio/wav">Your browser does not support the audio element.</audio></div>')    
     $('#pile').append(el)
 
     el.css('background', "rgb(" + r + ',' + g + ',' + b + ')')
-
-    jQuery.data(el[0], 'number', i)
 
     el.dblclick(function() { 
       this.children[1].play()
