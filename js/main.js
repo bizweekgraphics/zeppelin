b05cd298c383730bd6fa4dd7a614ea88904393a8
dyep49 <dyep49@gmail.com>
2014-05-12 14:05:54 -0400

    clips play in order on submit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var gameObject = {}
+
 $(document).ready(function() {
 
   var stairwayArray = [1,2,3,4]
@@ -9,14 +11,8 @@ $(document).ready(function() {
   for(var i=1;i<5;i++) {
     var num = stairwayArray.pop()
 
-    var r = Math.floor(255 * Math.random())
-    var g = Math.floor(255 * Math.random())
-    var b = Math.floor(255 * Math.random())
-
     var el = $('<div class="drag-item"><img src="img/spirit' + num + '.png"><audio controls><source src="audio/spirit' + num +'.wav" type="audio/wav">Your browser does not support the audio element.</audio></div>')    
     $('#pile').append(el)
-
-    el.css('background', "rgb(" + r + ',' + g + ',' + b + ')')
 
     el.dblclick(function() { 
       this.children[1].play()
@@ -24,6 +20,8 @@ $(document).ready(function() {
 
     var dropEl = $('<div class="drop">drop</div>')
     $('#drop').append(dropEl)
+
+    jQuery.data(dropEl[0], 'number', i)
   }
 
   $('.drag-item').draggable({
@@ -34,15 +32,8 @@ $(document).ready(function() {
   for(var i=1;i<5;i++) {
     var num = spiritArray.pop()
 
-
-    var r = Math.floor(255 * Math.random())
-    var g = Math.floor(255 * Math.random())
-    var b = Math.floor(255 * Math.random())
-
     var el = $('<div class="drag-item"><img src="img/stairway' + num + '.png"><audio controls><source src="audio/stairway' + num +'.wav" type="audio/wav">Your browser does not support the audio element.</audio></div>')    
     $('#pile').append(el)
-
-    el.css('background', "rgb(" + r + ',' + g + ',' + b + ')')
 
     el.dblclick(function() { 
       this.children[1].play()
@@ -56,9 +47,10 @@ $(document).ready(function() {
   })
 
   var dropEvent = function(event, ui) {
-    console.log('test')
+    var number = jQuery.data(this, 'number')
     ui.draggable.position( {of: $(this), my: 'left top', at: 'left top' })
     $(this).droppable('option', 'accept', ui.draggable);
+    gameObject[number] = $(ui.draggable)
   }
 
   $('.drop').droppable({
@@ -70,8 +62,47 @@ $(document).ready(function() {
   });
 
 
+// $('button').click(function() {
 
+// })
 
+// _.find($('.drag-item'), function(item) {
+// return item.offsetLeft === 338
+// }).children[1].play()
+
+  // $('button').click(function() {
+  //   for(var i=1; i<5; i++) {
+  //     var audio = gameObject[i].children()[1]
+  //     if(i < 4) {
+  //       audio.addEventListener('ended', function() {
+  //         debugger;
+  //         gameObject[i+1].children()[1].play()         
+  //       })      
+  //     }
+  //   }
+  // })
+  $('button').click(function() {
+    var audio = gameObject[1].children()[1]
+    audio.addEventListener('ended', function() {
+      gameObject[2].children()[1].play()
+    })
+
+    var audio2 = gameObject[2].children()[1]
+    audio2.addEventListener('ended', function() {
+      gameObject[3].children()[1].play()
+    })
+
+    var audio3 = gameObject[3].children()[1]
+    audio3.addEventListener('ended', function() {
+      gameObject[4].children()[1].play()
+    })
+  })
 
 })
+
+
+
+
+
+
 
