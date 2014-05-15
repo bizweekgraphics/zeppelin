a333c66c49f5d12236ff652352a4755314edb7b7
dyep49 <dyep49@gmail.com>
2014-05-15 15:01:45 -0400

    fixed facebook share

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var winningObject = {
 function fbShare(url, title, descr, image, winWidth, winHeight) {
     var winTop = (screen.height / 2) - (winHeight / 2);
     var winLeft = (screen.width / 2) - (winWidth / 2);
-    window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+    window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + 'http://www.businessweek.com/articles/2014-05-15/the-stairway-to-heaven-game-did-led-zeppelin-steal-the-greatest-song-opening-in-rock-history' + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
 }
 
 $(document).ready(function() {
